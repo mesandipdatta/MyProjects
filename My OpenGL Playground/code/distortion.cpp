27d83c73a761a67560f770ee2aba0307a50aa38a
Yu Sun <usunyu@gmail.com>
2015-08-10 01:16:16 -0700

    [opengl] distortion ready

--- My OpenGL Playground/code/distortion.cpp
+++ My OpenGL Playground/code/distortion.cpp
@@ -94,30 +94,59 @@ public:
 };
 
 /*-----------------------------------------------------------------------------
+ *  DISTORTION SHADER CODE
+ *-----------------------------------------------------------------------------*/
+const char * distortVert = GLSL(120,
+                         
+    attribute vec2 position;
+    attribute float vignette;
+    attribute vec2 textureCoordinate;                   //<-- Texture Coordinate Attribute
+                         
+    varying vec2 texCoord;                              //<-- To be passed to fragment shader
+                         
+    void main(void){
+        texCoord = textureCoordinate;
+                             
+        gl_Position = vec4(position, 0, 1);
+    }
+                
+);
+
+const char * distortFrag = GLSL(120,
+                         
+    varying vec2 texCoord;                            //<-- coordinate passed in from vertex shader
+                         
+    uniform sampler2D texture;                        //<-- The texture itself
+                         
+    void main(void){
+        gl_FragColor =  texture2D( texture, texCoord ); //<-- look up the coordinate
+    }
+                         
+);
+
+/*-----------------------------------------------------------------------------
  *  DISTORTION MESH
  *-----------------------------------------------------------------------------*/
 class DistortionMesh {
-public:
-    int BYTES_PER_FLOAT = 4;
-    int BYTES_PER_INT = 4;
-    int COMPONENTS_PER_VERT = 5;
-    int DATA_STRIDE_BYTES = 20;
-    
-    int DATA_POS_OFFSET = 0;
-    int DATA_VIGNETTE_OFFSET = 2;
-    int DATA_UV_OFFSET = 3;
     int ROWS = 40;
     int COLS = 40;
     
-    float VIGNETTE_SIZE_M_SCREEN = 0.002f;
-    
     int nIndices;
-    int mArrayBufferId = -1;
-    int mElementBufferId = -1;
+    GLuint mArrayBufferId = -1;
+    GLuint mElementBufferId = -1;
     
-    DistortionMesh(Window mWindow, Distortion distortion) {
-        float vertexData[ROWS * COLS * 5];
+    Distortion distortion;
+    
+public:
+    DistortionMesh() {
+    }
+    
+    DistortionMesh(Window mWindow) {
+        cout << "create the DistortionMesh" << endl;
+        
+        GLfloat vertexData[ROWS * COLS * 5];
         int vertexOffset = 0;
+        float scale = 500.0f;
         
         float xEyeOffsetMScreen = mWindow.width() / 2.0f;
         float yEyeOffsetMScreen = mWindow.height() / 2.0f;
@@ -141,18 +170,22 @@ public:
                 float uScreen = xScreen / mWindow.width();
                 float vScreen = yScreen / mWindow.height();
                 
-                vertexData[(vertexOffset + 0)] = (2.0f * uScreen - 1.0f);
-                vertexData[(vertexOffset + 1)] = (2.0f * vScreen - 1.0f);
+                vertexData[(vertexOffset + 0)] = (2.0f * uScreen - 1.0f) * scale;
+                
+                vertexData[(vertexOffset + 1)] = (2.0f * vScreen - 1.0f) * scale;
+                
                 vertexData[(vertexOffset + 2)] = 1.0f;
                 vertexData[(vertexOffset + 3)] = uTexture;
                 vertexData[(vertexOffset + 4)] = vTexture;
+                
+                cout << "(" << vertexData[(vertexOffset + 0)] << ":" << vertexData[(vertexOffset + 1)] << "), (" << vertexData[(vertexOffset + 3)] << ":" << vertexData[(vertexOffset + 4)] << ")" << endl;
                 
                 vertexOffset += 5;
             }
         }
         
         nIndices = ROWS * COLS * 2;
-        int indexData[nIndices];
+        GLuint indexData[nIndices];
         int indexOffset = 0;
         vertexOffset = 0;
         for (int row = 0; row < (ROWS - 1); row++) {
@@ -169,9 +202,9 @@ public:
                     }
                 }
                 indexData[(indexOffset++)] = vertexOffset;
-                indexData[(indexOffset++)] = (vertexOffset + 40);
+                indexData[(indexOffset++)] = (vertexOffset + COLS);
             }
-            vertexOffset += 40;
+            vertexOffset += ROWS;
         }
         
         GLuint bufferIds[2];
@@ -180,27 +213,16 @@ public:
         mElementBufferId = bufferIds[1];
         
         glBindBuffer(GL_ARRAY_BUFFER, mArrayBufferId);
-        glBufferData(GL_ARRAY_BUFFER, (ROWS * COLS * 5) * 4, vertexData, 35044);
+        glBufferData(GL_ARRAY_BUFFER, ROWS * COLS * 5 * sizeof(GLfloat), vertexData, GL_STATIC_DRAW);
         
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mElementBufferId);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, nIndices * 4, indexData, 35044);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, nIndices * sizeof(GLuint), indexData, GL_STATIC_DRAW);
         
+        // unbind
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
-};
-
-class EyeViewport {
-public:
-    float x;
-    float y;
-    float width;
-    float height;
-    float eyeX;
-    float eyeY;
     
-private:
-    EyeViewport() {}
 };
 
 /*-----------------------------------------------------------------------------
@@ -222,7 +244,6 @@ const char * vert = GLSL(120,
 
   void main(){
     dstColor = color;
-//    dstTextureCoord = textureCoord.xy * textureCoordScale;
     gl_Position = projection * view * model * position;   //<-- Apply transformation
   }
 
@@ -255,12 +276,18 @@ struct Vertex{
  *  OUR APP
  *-----------------------------------------------------------------------------*/
 struct MyApp : public App{
+    
+    /*-----------------------------------------------------------------------------
+     *  DISTORTION PARAM
+     *-----------------------------------------------------------------------------*/
+    GLint mOriginalFramebufferId[1];
+    GLint mViewport[4];
+    GLint mRenderbufferId = -1, mTextureId = -1, mFramebufferId = -1;
+    bool mProjectionChanged = true;
+    DistortionMesh mDistortionMesh;
 
     Shader * shader;
     
-    GLint mOriginalFramebufferId[1], mFramebufferId[1];
-    GLint mViewport[4];
-
     //ID of Vertex Attribute
     GLuint positionID, normalID, colorID;
 
@@ -273,10 +300,23 @@ struct MyApp : public App{
     GLuint modelID, viewID, projectionID;
 
     //Constructor (initialize application)
-    MyApp() { init(); }
+    MyApp() {
+        initDistortion();
+        init();
+    }
+    
+    void checkGlError(string op) {
+        int error;
+        if ((error = glGetError()) != 0) {
+            cout << op << ": glError " << error << endl;
+        }
+    }
+    
+    void initDistortion() {
+        mDistortionMesh = DistortionMesh(mWindow);
+    }
 
-    void init(){
-
+    void init() {
         //Specify the 8 VERTICES of A Cube
         Vertex cube[] = {
             {glm::vec3( 1, -1,  1), glm::vec4(1,0,0,1 )},
@@ -364,65 +404,114 @@ struct MyApp : public App{
         BINDVERTEXARRAY(0);
         glBindBuffer( GL_ARRAY_BUFFER, 0);
         glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0);
-
-
     }
-
-    void onDraw(){
-        beforeDrawFrame();
-
+    
+    void beforeDrawFrameDistortion() {
+        glGetIntegerv(GL_FRAMEBUFFER_BINDING, mOriginalFramebufferId);  // 36006
+        glGetIntegerv(GL_FRAMEBUFFER, &mFramebufferId);
+    }
+    
+    void afterDrawFrameDistortion() {
+        glBindFramebuffer(GL_FRAMEBUFFER, mOriginalFramebufferId[0]);
+        glViewport(0, 0, mWindow.width(), mWindow.height());
+    }
+    
+    GLuint setupRenderTextureAndRenderbuffer(int width, int height) {
+        mTextureId = createTexture(width, height);
+        checkGlError("setupRenderTextureAndRenderbuffer: create texture");
+        
+        GLuint renderbufferIds[1];
+        glGenRenderbuffers(1, renderbufferIds);
+        glBindRenderbuffer(GL_RENDERBUFFER, renderbufferIds[0]);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+        
+        mRenderbufferId = renderbufferIds[0];
+        checkGlError("setupRenderTextureAndRenderbuffer: create renderbuffer");
+        
+        GLuint framebufferIds[1];
+        glGenFramebuffers(1, framebufferIds);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebufferIds[0]);
+        mFramebufferId = framebufferIds[0];
+        
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mTextureId, 0);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderbufferIds[0]);
+        
+        int status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+        if (status != GL_FRAMEBUFFER_COMPLETE) {
+            cout << "Framebuffer is not complete: " << status << endl;
+        }
+        
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        
+        return framebufferIds[0];
+    }
+    
+    GLuint createTexture(int width, int height) {
+        GLuint textureIds[1];
+        glGenTextures(1, textureIds);
+        
+        glBindTexture(GL_TEXTURE_2D, textureIds[0]);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_SHORT_5_6_5, NULL);
+        
+        return textureIds[0];
+    }
+    
+    void onDrawFrameCustom() {
         static float time = 0.0;
         time += .01;
-
+        
         BINDVERTEXARRAY(arrayID);
-
+        
         glm::mat4 view = glm::lookAt( glm::vec3(0,0,5), glm::vec3(0,0,0), glm::vec3(0,1,0) );
         glm::mat4 proj = glm::perspective( PI / 3.0f, (float)window().width()/window().height(), 0.1f,-10.f);
-
+        
         glUniformMatrix4fv( viewID, 1, GL_FALSE, glm::value_ptr(view) );
         glUniformMatrix4fv( projectionID, 1, GL_FALSE, glm::value_ptr(proj) );
-
+        
         glm::mat4 model = glm::rotate( glm::mat4(), time, glm::vec3(0,1,0) );
         // glm::mat4 model = glm::rotate( glm::mat4(), 0.0f, glm::vec3(0,1,0) );
-      
+        
         // glm::mat4 translation =  glm::translate( glm::mat4(), glm::vec3(0,0,0) );
         // glm::mat4 rotation =  glm::rotate( glm::mat4(), 0.0f, glm::vec3(0,1,0) );
         // glm::mat4 scale = glm::scale( glm::mat4(), glm::vec3(1) );
         // //ORDER MATTERS!
         // glm::mat4 model = translation * rotation * scale;
-    
+        
         glUniformMatrix4fv( modelID, 1, GL_FALSE, glm::value_ptr(model) );
-    
+        
         //glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, elementID);
         glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, 0);
-      
+        
         BINDVERTEXARRAY(0);
+
+    }
+    
+    void onProjectionChanged() {
+        float zNear = 0.1f, zFar = -10.f;
         
-//        afterDrawFrame();
+//        mDistortionMesh =
+    }
+
+    void onDraw() {
+        if (mProjectionChanged) {
+            
+            mProjectionChanged = false;
+        }
+        
+        beforeDrawFrameDistortion();
+
+        onDrawFrameCustom();
+        
+        afterDrawFrameDistortion();
   
-    }
-    
-    void beforeDrawFrame() {
-        glGetIntegerv(GL_FRAMEBUFFER_BINDING, mOriginalFramebufferId);  // 36006
-        glGetIntegerv(GL_FRAMEBUFFER, mFramebufferId);
-    }
-    
-    void afterDrawFrame() {
-        glBindFramebuffer(GL_FRAMEBUFFER, mOriginalFramebufferId[0]);
-        glViewport(0, 0, mWindow.width(), mWindow.height());
-        
-        // 1) save context
-//        glGetIntegerv(GL_VIEWPORT, mViewport);
-//        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-//        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    }
-    
-    void renderDistortionMesh(DistortionMesh mesh) {
-        glBindBuffer(GL_ARRAY_BUFFER, mesh.mArrayBufferId);
-        glVertexAttribPointer(positionID, 3, GL_FLOAT, false, 20, 0 * 4);
-        glEnableVertexAttribArray(positionID);
-        
-//        glVertexAttribPointer(mProgramHolder.aVignette, 1, GLES20.GL_FLOAT, false, 20, 2 * 4);
     }
 
 };
--- My OpenGL Playground/xcode/My_OpenGL.build/Debug/ZERO_CHECK.build/Script-50D7D15E44134DDEBE59A8D0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/yusun/Repositories/MyProjects/My\ OpenGL\ Playground/xcode -f /Users/yusun/Repositories/MyProjects/My\ OpenGL\ Playground/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
