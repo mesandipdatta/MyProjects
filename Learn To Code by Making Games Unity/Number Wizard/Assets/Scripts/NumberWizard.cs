264273c502bf90d4543fbdf6675fd31f2797ff01
Yu Sun <usunyu@gmail.com>
2015-12-13 12:50:31 -0800

    [unity][lec 18] using if to make choices

--- Learn To Code by Making Games Unity/Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Learn To Code by Making Games Unity/Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -4,19 +4,30 @@ using System.Collections;
 public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		print ("Welcome to Number Wizard");
-		print ("Pick a number in your head, but don't tell me!");
+	void Start ()
+	{
+		print("Welcome to Number Wizard");
+		print("Pick a number in your head, but don't tell me!");
 
 		int max = 1000;
 		int min = 1;
 
-		print ("The highest number you can pick is " + max);
-		print ("The lowest number you can pick is " + min);
+		print("The highest number you can pick is " + max);
+		print("The lowest number you can pick is " + min);
+
+		print("Is the number higher or lower that 500?");
+		print("Up arrow = higher, down = lower, return = equal");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			print ("Up arrow pressed");
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Down arrow pressed");
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won");
+		}
 	}
 }
