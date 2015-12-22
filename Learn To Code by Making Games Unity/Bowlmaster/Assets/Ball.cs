fdf6583fa48ed97a64764369e481df88a5dde4d8
Yu Sun <usunyu@gmail.com>
2015-12-21 22:27:02 -0800

    [unity][lec 179~182] blender & 3d sphere as rolling ball

---
+++ Learn To Code by Making Games Unity/Bowlmaster/Assets/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float launchSpeed;
+
+	private Rigidbody rigidBoday;
+	private AudioSource audioSource;
+
+	public void Launch ()
+	{
+		rigidBoday.velocity = new Vector3 (0, 0, launchSpeed);
+		audioSource.Play ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigidBoday = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
